d15c8bdeaf1ee6847af27d2032486ac45ee98c56
Arihant Lunawat <arihantb2@gmail.com>
2026-02-06 08:31:23 +1100

    Revert deploy script to connors branch
    Signed-off-by: Arihant Lunawat <arihantb2@gmail.com>

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -15,8 +15,6 @@ import torchvision.transforms as transforms
 script_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(script_path, "../PenguinPi-robot/software/python/client/")))
 from pibot_client import PiBot
-from steer_labels import LABELS, steering_to_class, label_to_class, LABEL_TO_ANGLE
-from controller import Controller
 
 
 parser = argparse.ArgumentParser(description='PiBot client')
@@ -25,7 +23,6 @@ args = parser.parse_args()
 
 bot = PiBot(ip=args.ip)
 
-<<<<<<< HEAD
 # stop the robot 
 bot.setVelocity(0, 0)
 
@@ -34,51 +31,6 @@ bot.setVelocity(0, 0)
 #LOAD NETWORK WEIGHTS HERE
 
 #countdown before beginning
-=======
-controller = Controller()
-
-# stop the robot 
-bot.setVelocity(0, 0)
-
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.fc1 = nn.Linear(1344, 256)
-        self.fc2 = nn.Linear(256, 5)
-
-        self.relu = nn.ReLU()
-
-
-    def forward(self, x):
-        #extract features with convolutional layers
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        
-        #linear layer for classification
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-       
-        return x
-    
-net = Net()
-
-#LOAD NETWORK WEIGHTS HERE
-net.load_state_dict(torch.load('steer_net.pth'))
-
-# Stop detection state
-consecutive_stop_count = 0
-stop_start_time = None
-ignore_stop_until = None
-
-# Countdown before beginning
->>>>>>> clan2098/test_stop_network
 print("Get ready...")
 time.sleep(1)
 print("3")
@@ -97,44 +49,20 @@ try:
 
         #TO DO: apply any necessary image transforms
 
-<<<<<<< HEAD
         #TO DO: pass image through network get a prediction
 
         #TO DO: convert prediction into a meaningful steering angle
 
         #TO DO: check for stop signs?
-=======
-        #TO DO: apply any necessary image transforms
-        transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Resize((40, 60)),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                                ])
-        img = transform(im) 
-
-
-
-        #TO DO: pass image through network get a prediction
-        outputs = net(img.unsqueeze(0)) # add batch dimension
-        _, prediction = torch.max(outputs, 1)
-
-        #TO DO: convert prediction into a meaningful steering angle
-        # angle = LABEL_TO_ANGLE[prediction.item()]
-
-        left,right = controller(prediction.item())
-
-        #TODO: convert prediction into a meaningful steering angle
->>>>>>> clan2098/test_stop_network
         
-        # angle = 0
+        angle = 0
 
-        # Kd = 20 #base wheel speeds, increase to go faster, decrease to go slower
-        # Ka = 20 #how fast to turn when given an angle
-        # left  = int(Kd + Ka*angle)
-        # right = int(Kd - Ka*angle)
+        Kd = 20 #base wheel speeds, increase to go faster, decrease to go slower
+        Ka = 20 #how fast to turn when given an angle
+        left  = int(Kd + Ka*angle)
+        right = int(Kd - Ka*angle)
             
         bot.setVelocity(left, right)
-        #bot.setVelocity(0, 0)
-        print(f"Predicted class: {prediction.item()}, controller_angle: {controller.angle:.2f}, left: {left}, right: {right}")
             
         
 except KeyboardInterrupt:    
